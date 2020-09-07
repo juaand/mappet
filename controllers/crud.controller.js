8b3987d4956a1689f11d50dc4e1c30c4d6784574
Andres Martinez <andresmartgonz@gmail.com>
2020-09-07 17:59:56 +0200

    crud.controller misstape

--- controllers/crud.controller.js
+++ controllers/crud.controller.js
@@ -6,166 +6,161 @@ const Pet = require('../models/pet.model')
 const Comment = require('../models/comment.model')
 const bcryptjs = require('bcryptjs')
 const saltRounds = 10
-const fetch = require("node-fetch");
+const fetch = require('node-fetch')
 
 ////////////////////////////////////////////////////////////////////////
 //////////////////////////// RENDER USER ///////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
 module.exports.editUser = (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   User.findById(id)
     .then((user) => {
       // res.json(user)
-      const userId = req.session.currentUser._id;
+      const userId = req.session.currentUser._id
       if (userId === id) {
-        res.render("users/update-profile", { user });
+        res.render('users/update-profile', { user })
       } else {
-        req.session.destroy();
-        res.render("auth/login", {
-          message: "Something is wrong with your user, please login again.",
-        });
+        req.session.destroy()
+        res.render('auth/login', {
+          message: 'Something is wrong with your user, please login again.'
+        })
       }
     })
-    .catch((error) => next(error));
-};
+    .catch((error) => next(error))
+}
 
 ////////////////////////////////////////////////////////////////////////
 //////////////////////////// EDIT USER /////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
 module.exports.saveEditedUser = (req, res, next) => {
-  const { username, email, avatar, password, bio, name } = req.body;
-  const id = req.params.id;
-  req.body.avatar = req.file ? req.file.filename : undefined;
+  const { username, email, avatar, password, bio, name } = req.body
+  const id = req.params.id
+  req.body.avatar = req.file ? req.file.filename : undefined
   if (req.body.avatar) {
     User.findByIdAndUpdate(id, {
       name: req.body.name,
       avatar: `${process.env.CLOUDINARY_SECURE}/${req.body.avatar}`,
       // password: hashedPassword,
-      bio: req.body.bio,
+      bio: req.body.bio
     })
       .then(() => {
-        res.redirect(`/user-profile/${id}`);
+        res.redirect(`/user-profile/${id}`)
       })
-      .catch((error) => next(error));
+      .catch((error) => next(error))
   } else {
     User.findByIdAndUpdate(id, {
       name: req.body.name,
       // password: hashedPassword,
-      bio: req.body.bio,
+      bio: req.body.bio
     })
       .then(() => {
-        res.redirect(`/user-profile/${id}`);
+        res.redirect(`/user-profile/${id}`)
       })
-      .catch((error) => next(error));
+      .catch((error) => next(error))
   }
-};
+}
 
 ////////////////////////////////////////////////////////////////////////
 //////////////////////////// DELETE USER /////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
 module.exports.deleteUser = (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   User.findByIdAndDelete(id)
     .then(() => {
       if (req.session.currentUser.role === 'ADMIN') {
         res.redirect('/admin/stats/users')
       } else {
-        req.session.destroy();
-        res.redirect("/");
+        req.session.destroy()
+        res.redirect('/')
       }
     })
-    .catch((error) => next(error));
-};
+    .catch((error) => next(error))
+}
 
 ////////////////////////////////////////////////////////////////////////
 //////////////////////////// USER SPOTS ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
 module.exports.createSpot = (req, res, next) => {
-  res.render("spots/new-spot", { title: "Mappet your spot!" });
-};
+  res.render('spots/new-spot', { title: 'Mappet your spot!' })
+}
 module.exports.saveSpot = (req, res, next) => {
-  const id = req.params.id;
-  const name = req.body.name;
-  const categories = typeof (req.body.categories) === 'string' ? [req.body.categories] : req.body.categories;
-  const subcategories = typeof (req.body.subcategories) === 'string' ? [req.body.subcategories] : req.body.subcategories;
-
+  const id = req.params.id
+  const name = req.body.name
+  const categories =
+    typeof req.body.categories === 'string' ? [req.body.categories] : req.body.categories
+  const subcategories =
+    typeof req.body.subcategories === 'string' ? [req.body.subcategories] : req.body.subcategories
 
   if (!categories) {
-    res.render("spots/new-spot", {
-      title: "Mappet your spot!",
-      message: "Please select at least one category",
-    });
-    return;
+    res.render('spots/new-spot', {
+      title: 'Mappet your spot!',
+      message: 'Please select at least one category'
+    })
+    return
   }
   if (!name) {
-    res.render("spots/new-spot", {
-      message: "Please provide a name.",
-      title: "Mappet your spot!",
-    });
-    return;
+    res.render('spots/new-spot', {
+      message: 'Please provide a name.',
+      title: 'Mappet your spot!'
+    })
+    return
   }
 
   const urlAddress = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.address}&key=AIzaSyBa0-rQW4YzCsHAuNfsOjUXMsDOpSpPBZs`
 
-  fetch(urlAddress.normalize('NFD')
-  .replace(/[\u0300-\u036f]/g, ''))
-  .then(response => response.json())
-   .then((myJson) => {
-    return Spot.create({
-      lat: myJson.results[0].geometry.location.lat,
-      lng: myJson.results[0].geometry.location.lng,
-      name: req.body.name,
-      content: req.body.content,
-      creatorId: id,
-      pictures: req.files
-        ? req.files.map(
-            (file) => `${process.env.CLOUDINARY_SECURE}/${file.filename}`
-          )
-        : "",
-      url: req.body.url,
-      // category: [...new Set(req.body.categories)],
-      category: req.body.categories, 
-      subCategory: req.body.subcategories,
-      address: req.body.address,
-      city: req.body.city,
-      zipCode: req.body.zipcode,
-      days: req.body.days,
-      open: req.body.open,
-      close: req.body.close,
-      instagram: req.body.instagram,
-      facebook: req.body.facebook,
-      email: req.body.email,
-      phone: req.body.phone,
-    })
-      .then(() => {
-        res.redirect(`/`);
+  fetch(urlAddress.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
+    .then((response) => response.json())
+    .then((myJson) => {
+      return Spot.create({
+        lat: myJson.results[0].geometry.location.lat,
+        lng: myJson.results[0].geometry.location.lng,
+        name: req.body.name,
+        content: req.body.content,
+        creatorId: id,
+        pictures: req.files ? req.files.map(
+              (file) => `${process.env.CLOUDINARY_SECURE}/${file.filename}`
+            )
+          : '',
+        url: req.body.url,
+        // category: [...new Set(req.body.categories)],
+        category: req.body.categories,
+        subCategory: req.body.subcategories,
+        address: req.body.address,
+        city: req.body.city,
+        zipCode: req.body.zipcode,
+        days: req.body.days,
+        open: req.body.open,
+        close: req.body.close,
+        instagram: req.body.instagram,
+        facebook: req.body.facebook,
+        email: req.body.email,
+        phone: req.body.phone
       })
-      .catch((error) => next(error));
-   })
-
-  
-};
+        .then(() => {
+          res.redirect(`/`)
+        })
+        .catch((error) => next(error))
+    })
 }
-
 
 ////////////////////////////////////////////////////////////////////////
 //////////////////////////// EDIT SPOTS ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
 module.exports.editSpot = (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   Spot.findById(id).then((spot) => {
     res.render('spots/edit', { spot, title: 'Edit ' })
   })
 }
 
 module.exports.updateSpot = (req, res, next) => {
-  const id = req.params.id;
-  const category = req.params.category;
+  const id = req.params.id
+  const category = req.params.category
   if (req.body.pictures) {
     Spot.findByIdAndUpdate(id, {
       name: req.body.name,
@@ -174,7 +169,7 @@ module.exports.updateSpot = (req, res, next) => {
       pictures: req.files ? req.files.map(
             (file) => `${process.env.CLOUDINARY_SECURE}/${file.filename}`
           )
-        : "",
+        : '',
       url: req.body.url,
       category: req.body.categories,
       address: req.body.address,
@@ -186,13 +181,13 @@ module.exports.updateSpot = (req, res, next) => {
       instagram: req.body.instagram,
       facebook: req.body.facebook,
       email: req.body.email,
-      phone: req.body.phone,
+      phone: req.body.phone
     })
       .then(() => {
-        console.log(id);
-        res.redirect(`/${category}/${id}`);
+        console.log(id)
+        res.redirect(`/${category}/${id}`)
       })
-      .catch((error) => next(error));
+      .catch((error) => next(error))
   } else {
     Spot.findByIdAndUpdate(id, {
       name: req.body.name,
@@ -209,27 +204,28 @@ module.exports.updateSpot = (req, res, next) => {
       instagram: req.body.instagram,
       facebook: req.body.facebook,
       email: req.body.email,
-      phone: req.body.phone,
+      phone: req.body.phone
     })
       .then(() => {
-        res.redirect(`/${category}/${id}`);
+        res.redirect(`/${category}/${id}`)
       })
-      .catch((error) => next(error));
+      .catch((error) => next(error))
   }
 }
 
 module.exports.deleteSpot = (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   Spot.findByIdAndDelete(id)
     .then(() => {
       if (req.session.currentUser.role === 'ADMIN') {
         res.redirect('/admin/stats/spots')
       } else {
-        res.redirect("/");
+        res.redirect('/')
       }
     })
-    .catch((error) => next(error));
-};
+    .catch((error) => next(error))
+}
+
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////// ADD PET /////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
@@ -238,15 +234,15 @@ module.exports.addPet = (req, res, next) => {
 }
 
 module.exports.createPet = (req, res, next) => {
-  const id = req.params.id;
-  req.body.avatar = req.file ? req.file.filename : undefined;
+  const id = req.params.id
+  req.body.avatar = req.file ? req.file.filename : undefined
   Pet.create({
     creatorId: id,
     animal: req.body.animal,
     name: req.body.name,
     avatar: `${process.env.CLOUDINARY_SECURE}/${req.body.avatar}`,
     age: req.body.age,
-    breed: req.body.breed,
+    breed: req.body.breed
   }).then((pet) => {
     res.render('pets/add-pet', {
       title: 'add your pet',
@@ -260,45 +256,45 @@ module.exports.createPet = (req, res, next) => {
 ////////////////////////////////////////////////////////////////////////
 
 module.exports.editPet = (req, res, next) => {
-  const petId = req.params.id;
+  const petId = req.params.id
   Pet.findById(petId)
     .then((pet) => {
-      res.render("pets/edit-pet", { pet, title: "Edit pet" });
+      res.render('pets/edit-pet', { pet, title: 'Edit pet' })
     })
     .catch((error) => next(error))
 }
 
 module.exports.updatePet = (req, res, next) => {
-  const petId = req.params.id;
-  req.body.avatar = req.file ? req.file.filename : undefined;
+  const petId = req.params.id
+  req.body.avatar = req.file ? req.file.filename : undefined
   if (req.body.avatar) {
     Pet.findByIdAndUpdate(petId, {
       name: req.body.name,
       animal: req.body.animal,
       avatar: `${process.env.CLOUDINARY_SECURE}/${req.body.avatar}`,
       age: req.body.age,
-      breed: req.body.breed,
+      breed: req.body.breed
     })
       .then((pet) => {
-        res.redirect(`/user/${pet.creatorId._id}`);
+        res.redirect(`/user/${pet.creatorId._id}`)
       })
-      .catch((error) => next(error));
+      .catch((error) => next(error))
   } else {
     Pet.findByIdAndUpdate(petId, {
       name: req.body.name,
       animal: req.body.animal,
       age: req.body.age,
-      breed: req.body.breed,
+      breed: req.body.breed
     })
       .then((pet) => {
-        res.redirect(`/user/${pet.creatorId._id}`);
+        res.redirect(`/user/${pet.creatorId._id}`)
       })
-      .catch((error) => next(error));
+      .catch((error) => next(error))
   }
 }
 
 module.exports.deletePet = (req, res, next) => {
-  const petId = req.params.id;
+  const petId = req.params.id
   Pet.findByIdAndDelete(petId)
     .then(() => {
       const user = req.session.currentUser
@@ -312,15 +308,15 @@ module.exports.deletePet = (req, res, next) => {
 }
 
 module.exports.changePassword = (req, res, next) => {
-  const id = req.params.id;
-  const userPass = req.session.currentUser.password;
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  const id = req.params.id
+  const userPass = req.session.currentUser.password
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
   if (!regex.test(req.body.newpassword)) {
-    res.status(500).render("users/update-profile", {
+    res.status(500).render('users/update-profile', {
       message:
-        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
-    });
-    return;
+        'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.'
+    })
+    return
   }
   bcryptjs
     .compare(req.body.password, userPass)
@@ -332,21 +328,20 @@ module.exports.changePassword = (req, res, next) => {
           .then((newHashedPassword) => {
             // res.json(id)
             return User.findByIdAndUpdate(id, {
-              password: newHashedPassword,
-            });
+              password: newHashedPassword
+            })
           })
           .then(() => {
             res.render(`users/update-profile`, {
-              message: "password changed successfully",
-            });
+              message: 'password changed successfully'
+            })
           })
-          .catch((error) => next(error));
+          .catch((error) => next(error))
       } else {
         res.render(`users/update-profile`, {
-          message: "current password is invalid",
-        });
+          message: 'current password is invalid'
+        })
       }
     })
     .catch((error) => next(error))
 }
-
