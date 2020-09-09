255d6dfa99b887a42b8b798b25b6bcdeeb50747e
Juan Romero <jrg89@hotmail.es>
2020-09-09 19:10:14 +0200

    task: js045

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,145 +1,149 @@
-//User graph
-// axios
-//   .get(usersJson)
-//   .then((responseFromAPI) => {
-//     console.log(responseFromAPI.data)
+const usersJson = '/admin/data/users'
+const spotsJson = '/admin/data/spots'
 
-//     let dates = []
-//     let numberOfUsers = []
-//     responseFromAPI.data.forEach((el) => {
-//       dates.push(new Date(el.createdAt).toDateString())
-//     })
-//     const numberOfDates = dates.reduce((el, date) => {
-//       el[date] = (el[date] || 0) + 1
-//       return el
-//     }, {})
-//     numberOfUsers = Object.values(numberOfDates)
+// User graph
 
-//     console.log(dates)
-//     console.log(numberOfUsers)
+ axios
+   .get(usersJson)
+   .then((responseFromAPI) => {
+     console.log(responseFromAPI.data)
 
-//     new Chart(document.getElementById('my-chart'), {
-//       type: 'line',
-//       data: {
-//         labels: [...new Set(dates)],
-//         datasets: [
-//           {
-//             label: 'Number of users',
-//             borderColor: '#04adbf',
-//             fill: false,
-//             data: numberOfUsers
-//           }
-//         ]
-//       },
-//       options: {
-//         scales: {
-//           yAxes: [
-//             {
-//               ticks: {
-//                 beginAtZero: true,
-//                 stepSize: 1
-//               }
-//             }
-//           ]
-//         },
-//         legend: { display: false },
-//         title: {
-//           display: true,
-//           text: 'Users per day'
-//         }
-//       }
-//     })
-//   })
-//   .catch((err) => console.log('Error while getting the data: ', err))
+     let dates = []
+     let numberOfUsers = []
+     responseFromAPI.data.forEach((el) => {
+       dates.push(new Date(el.createdAt).toDateString())
+     })
+     const numberOfDates = dates.reduce((el, date) => {
+       el[date] = (el[date] || 0) + 1
+       return el
+     }, {})
+     numberOfUsers = Object.values(numberOfDates)
 
-// //Projects & Users graph
-// axios
-//   .all([axios.get(usersJson), axios.get(projectsJson)])
-//   .then(
-//     axios.spread(function (usersData, projectsData) {
-//       console.log(usersData.data)
-//       console.log(projectsData.data)
+     console.log(dates)
+     console.log(numberOfUsers)
 
-//       let userDates = []
-//       let numberOfUsers = []
-//       usersData.data.forEach((el) => {
-//         userDates.push(new Date(el.createdAt).getMonth())
-//       })
-//       const numberOfDates = userDates.reduce((el, date) => {
-//         el[date] = (el[date] || 0) + 1
-//         return el
-//       }, {})
+     new Chart(document.getElementById('my-chart'), {
+       type: 'line',
+       data: {
+         labels: [...new Set(dates)],
+         datasets: [
+           {
+             label: 'Number of users',
+             borderColor: '#04adbf',
+             fill: false,
+             data: numberOfUsers
+           }
+         ]
+       },
+       options: {
+         scales: {
+           yAxes: [
+             {
+               ticks: {
+                 beginAtZero: true,
+                 stepSize: 1
+               }
+             }
+           ]
+         },
+         legend: { display: false },
+         title: {
+           display: true,
+           text: 'Users per day'
+         }
+       }
+     })
+   })
+   .catch((err) => console.log('Error while getting the data: ', err))
 
-//       numberOfUsers = Object.values(numberOfDates)
+// Spots & Users graph
+ axios
+   .all([axios.get(usersJson), axios.get(spotsJson)])
+   .then(
+     axios.spread(function (usersData, spotsData) {
+       console.log(usersData.data)
+       console.log(spotsData.data)
 
-//       console.log(userDates)
-//       console.log(numberOfUsers)
+       let userDates = []
+       let numberOfUsers = []
+       usersData.data.forEach((el) => {
+         userDates.push(new Date(el.createdAt).getMonth())
+       })
+       const numberOfDates = userDates.reduce((el, date) => {
+         el[date] = (el[date] || 0) + 1
+         return el
+       }, {})
 
-//       let projectDates = []
-//       let numberOfProjects = []
-//       projectsData.data.forEach((el) => {
-//         projectDates.push(new Date(el.createdAt).getMonth())
-//       })
-//       const numberOfUserDates = projectDates.reduce((el, date) => {
-//         el[date] = (el[date] || 0) + 1
-//         return el
-//       }, {})
+       numberOfUsers = Object.values(numberOfDates)
 
-//       numberOfProjects = Object.values(numberOfUserDates)
+       console.log(userDates)
+       console.log(numberOfUsers)
 
-//       console.log(projectDates)
-//       console.log(numberOfProjects)
+       let spotDates = []
+       let numberOfSpots = []
+       spotsData.data.forEach((el) => {
+         spotDates.push(new Date(el.createdAt).getMonth())
+       })
+       const numberOfUserDates = spotDates.reduce((el, date) => {
+         el[date] = (el[date] || 0) + 1
+         return el
+       }, {})
 
-//       new Chart(document.getElementById('mix-canvas'), {
-//         type: 'line',
-//         data: {
-//           labels: [
-//             'Jan',
-//             'Feb',
-//             'Mar',
-//             'Apr',
-//             'May',
-//             'Jun',
-//             'Jul',
-//             'Aug',
-//             'Sep',
-//             'Oct',
-//             'Nov',
-//             'Dec'
-//           ],
-//           datasets: [
-//             {
-//               label: 'Number of users',
-//               borderColor: '#04adbf',
-//               fill: false,
-//               data: numberOfUsers
-//             },
-//             {
-//               label: 'Number of project',
-//               borderColor: '#a0a603',
-//               fill: false,
-//               data: numberOfProjects
-//             }
-//           ]
-//         },
-//         options: {
-//           scales: {
-//             yAxes: [
-//               {
-//                 ticks: {
-//                   beginAtZero: true,
-//                   stepSize: 1
-//                 }
-//               }
-//             ]
-//           },
-//           legend: { display: false },
-//           title: {
-//             display: true,
-//             text: 'Users & Projects per day'
-//           }
-//         }
-//       })
-//     })
-//   )
-//   .catch((err) => console.log('Error while getting the data: ', err))
+       numberOfSpots = Object.values(numberOfUserDates)
+
+       console.log(spotDates)
+       console.log(numberOfSpots)
+
+       new Chart(document.getElementById('mix-canvas'), {
+         type: 'line',
+         data: {
+           labels: [
+             'Jan',
+             'Feb',
+             'Mar',
+             'Apr',
+             'May',
+             'Jun',
+             'Jul',
+             'Aug',
+             'Sep',
+             'Oct',
+             'Nov',
+             'Dec'
+           ],
+           datasets: [
+             {
+               label: 'Number of users',
+               borderColor: '#04adbf',
+               fill: false,
+               data: numberOfUsers
+             },
+             {
+               label: 'Number of spots',
+               borderColor: '#a0a603',
+               fill: false,
+               data: numberOfSpots
+             }
+           ]
+         },
+         options: {
+           scales: {
+             yAxes: [
+               {
+                 ticks: {
+                   beginAtZero: true,
+                   stepSize: 10
+                 }
+               }
+             ]
+           },
+           legend: { display: false },
+           title: {
+             display: true,
+             text: 'Users & Spots per day'
+           }
+         }
+       })
+     })
+   )
+   .catch((err) => console.log('Error while getting the data: ', err))
